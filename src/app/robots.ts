addf602eb3bebdb2eaca893c1fd5e9ee327412dd
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2026-01-11 18:25:02 +0000

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = (process.env.SITE_URL || "http://localhost:3000").replace(/\/+$/, "");
+  const siteUrl = (process.env.SITE_URL || "https://www.spifex.com").replace(/\/+$/, "");
   return {
     rules: [{ userAgent: "*", allow: "/" }],
     sitemap: `${siteUrl}/sitemap.xml`,
